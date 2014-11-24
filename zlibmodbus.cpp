627884c87446e3bd78dd3d3387b439b2f26010f8
Maximus Oozenthor <Github@Wideopen.Biz>
2014-11-25 09:29:25 +1100

    v2

---
+++ zlibmodbus.cpp
@@ -0,0 +1,5 @@
+#include "zlibmodbus.h"
+
+ZLibModbus::ZLibModbus()
+{
+}
---
+++ zlibmodbus.h
@@ -0,0 +1,10 @@
+#ifndef ZLIBMODBUS_H
+#define ZLIBMODBUS_H
+
+class ZLibModbus
+{
+public:
+    ZLibModbus();
+};
+
+#endif // ZLIBMODBUS_H
